f34b1807adbc928b689d138a3ef49d7a7a9ec96e
Fatih Kayacı <fatihkayaci5334@gmail.com>
2024-06-09 02:13:53 +0300

    Kalıtım örneği

---
+++ Kalıtım/Kuslar.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Kalıtım
+{
+    public class Kuslar
+    {
+        public string tur;
+        public string ses;
+        public int hiz;
+        public double agirlik;
+    }
+}
---
+++ Kalıtım/Program.cs
@@ -0,0 +1,25 @@
+﻿using Kalıtım;
+
+namespace Algoritma3;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Papagan p = new Papagan();
+        p.tur = "papağan";
+        p.hiz = 50;
+        p.isim = "sultan";
+        p.renk = "sarı-yeşil";
+        p.ses = "cik";
+        p.agirlik = 1650;
+
+        Console.WriteLine(p.tur);
+        Console.WriteLine(p.isim);
+        Console.WriteLine(p.hiz);
+        Console.WriteLine(p.renk);
+        Console.WriteLine(p.ses);
+        Console.WriteLine(p.agirlik);
+        Console.ReadLine();
+    }
+}
